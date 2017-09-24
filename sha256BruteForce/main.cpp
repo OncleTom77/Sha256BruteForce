dfbed7920ff7966a1bf81a06a2f250b6d2d56de6
thomasfouan <thomasfouan@hotmail.fr>
2017-09-24 17:40:08 +0200

    Initial Commit

---
+++ sha256BruteForce/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sha256BruteForce
+//
+//  Created by Thomas Fouan on 24/09/2017.
+//  Copyright © 2017 Thomas Fouan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
